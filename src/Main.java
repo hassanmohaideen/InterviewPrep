51b4a88802f05612ec2ed423c0ca5b39619d71f7
Hassan Mohaideen <2443106@wholefoods.com>
2023-11-21 12:55:23 -0500

    Combiniation sum 2 finished

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import graphs.DirectedGraph;
 import sorting.QuickSort;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -17,6 +18,7 @@ public class Main {
     public static List<List<Integer>> combinationSum2(int[] candidates, int target) {
         HashSet<List<Integer>> ans = new HashSet<List<Integer>>();
         LinkedList<Integer> curr = new LinkedList<Integer>();
+        Arrays.sort(candidates);
         backTracking(ans, curr, candidates, target, 0);
         return new ArrayList<>(ans);
     }
@@ -29,11 +31,11 @@ public class Main {
             int pos
     ) {
         if (target == 0) {
-            ArrayList toadd = new ArrayList<>(curr);
-            Collections.sort(toadd);
-            ans.add(toadd);
+            ans.add(new ArrayList<>(curr));
         } else if (target > 0) {
             for (int i=pos; i<candidates.length; i++) {
+                if (i != pos && candidates[i] == candidates[i - 1])
+                    continue;
                 curr.addLast(candidates[i]);
                 backTracking(ans, curr, candidates, target-candidates[i], i+1);
                 curr.removeLast();
